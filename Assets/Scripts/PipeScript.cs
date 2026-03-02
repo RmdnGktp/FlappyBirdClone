2ef00018d1165b74617e3aa47e14bc245fc35b49
Ramadan <ramadanclub@gmail.com>
2026-03-02 12:52:15 +0100

    feat: Implement Bird Flap Rotation Mechanic

--- Assets/Scripts/PipeScript.cs
+++ Assets/Scripts/PipeScript.cs
@@ -20,6 +20,14 @@ public class PipeScript : MonoBehaviour
             // or
             // transform.Translate (Vector3.left * moveSpeed * Time.deltaTime);
         }
+        else
+        {
+            BoxCollider2D[] colliders = GetComponentsInChildren<BoxCollider2D>();
+            foreach ( BoxCollider2D col in colliders)
+            {
+                col.enabled = false;
+            }  
+        }
 
         if (transform.position.x < deathZone)
         {
